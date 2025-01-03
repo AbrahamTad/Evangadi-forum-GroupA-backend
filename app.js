7455bcfa23c2ed6db013b1872d331b59eb3e30e4
Your Name <you@example.com>
2025-01-03 04:14:16 +0100

    backend app.js updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const port = 5500
+const port = process.env.PORT || 5500;
+// const port = 5500
 const cors = require("cors")
 
 app.use(cors())
