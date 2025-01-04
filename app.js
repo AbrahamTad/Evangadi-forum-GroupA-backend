b14e9cdeb43d2233d24c25869ccbf6cccb805a94
Your Name <you@example.com>
2025-01-04 03:32:26 +0100

    middleware

--- app.js
+++ app.js
@@ -116,6 +116,7 @@
 //   console.log(`Server is running on port ${PORT}`);
 // });
 
+// require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -128,7 +129,7 @@ const PORT = process.env.PORT || 5000; // Use Render's provided PORT or default 
 
 // Database connection (ensure this is configured properly)
 const dbConnection = require("./db/dbConfig");
-
+const authMiddleware = require("./middleware/authMiddleware");
 // Import routes
 const userRoute = require("./routes/userRoute");
 const questionRoute = require("./routes/questionRoute");
